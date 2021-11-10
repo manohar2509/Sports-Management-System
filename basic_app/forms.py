0089d40c591d3547187870c248f9d4abeda53f74
manohar2509 <chundurimanohar2509@gmail.com>
2021-11-11 01:31:39 +0530

    chatbox completed

---
+++ basic_app/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from django.contrib.auth.models import User
+from basic_app.models import UserProfileInfo,Chat
+from datetimepicker.widgets import DateTimePicker
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget = forms.PasswordInput())
+    class Meta:
+        model = User
+        fields = ('username','email','password')
+class UserProfileInfoForm(forms.ModelForm):
+    class Meta:
+        model = UserProfileInfo
+        fields = ('portofolio_site','profile_pic','country','sport')
+class ChatForm(forms.Form):
+    message=forms.CharField(max_length=300, required=True)
+class availform(forms.Form):
+    check_in = forms.DateTimeField(required=True,input_formats=['%Y-%m-%d %H:%M',])
+    check_out = forms.DateTimeField(required=True,input_formats=['%Y-%m-%d %H:%M',])
